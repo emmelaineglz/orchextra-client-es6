ebd06f4ebca635f4140cc3881b56649e7a2095fe
Ethel Gonzalez <ethel.gonzalez@gigigo.com.mx>
2017-06-14 13:43:58 -0500

    correct indentation

--- src/User.js
+++ src/User.js
@@ -1,60 +1,60 @@
 export default class User {
-    /**
-     *
-     * @param user
-     */
-    constructor(user){
-        this.user = user
-    }
-    /**
-     *
-     * @return languageCode
-     */
-    get languageCode(){
-        return this.user.languageCode
-    }
-    /**
-     *
-     * @returns role
-     */
-    get role(){
-        return this.user.role
-    }
-    /**
-     *
-     * @returns password
-     */
-    get password(){
-        return this.user.password
-    }
-    /**
-     *
-     * @return email
-     */
-    get email(){
-        return this.user.email
-    }
+  /**
+   *
+   * @param user
+   */
+  constructor (user) {
+    this.user = user
+  }
+  /**
+   *
+   * @return languageCode
+   */
+  get languageCode () {
+    return this.user.languageCode
+  }
+  /**
+   *
+   * @returns role
+   */
+  get role () {
+    return this.user.role
+  }
+  /**
+   *
+   * @returns password
+   */
+  get password () {
+    return this.user.password
+  }
+  /**
+   *
+   * @return email
+   */
+  get email () {
+    return this.user.email
+  }
 
-    /**
-     * @return name
-     */
-    get name(){
-        return this.user.name
-    }
+  /**
+   * @return name
+   */
+  get name () {
+    return this.user.name
+  }
 
-    /**
-     *
-     * @return username
-     */
-    get username(){
-        return this.user.username
-    }
+  /**
+   *
+   * @return username
+   */
+  get username () {
+    return this.user.username
+  }
 
-    /**
-     *
-     * @returns projectIds
-     */
-    get projectIds(){
-        return this.user.projectIds
-    }
+  /**
+   *
+   * @returns projectIds
+   */
+  get projectIds () {
+    return this.user.projectIds
+  }
 }
--- test/index.js
+++ test/index.js
@@ -4,17 +4,17 @@ var auth = new Auth('https://auth-api-coupons.s.gigigoapps.com')
 
 /** PROMESAS **/
 auth.authClient('qwerty', 'qwerty')
-    .then(client => {
-        console.log('Token:', client.getToken())
-        console.log('ExpiresIn:', client.getExpiresIn())
-    })
+  .then(client => {
+    console.log('Token:', client.getToken())
+    console.log('ExpiresIn:', client.getExpiresIn())
+  })
 
 auth.loginUser('nehomar.correa', 'gigigo10')
-    .then(user => {
-        console.log('Nombre:', user.name)
-        console.log('Email:', user.email)
-        console.log('Username:', user.username)
-    })
-    .catch(error => {
-        console.log('ErrorStatusCode:', error.statusCode)
-    })
+  .then(user => {
+    console.log('Nombre:', user.name)
+    console.log('Email:', user.email)
+    console.log('Username:', user.username)
+  })
+  .catch(error => {
+    console.log('ErrorStatusCode:', error.statusCode)
+  })
